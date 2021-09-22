97e98e7d5dfc9d77c70dd7318130f68b03e6c70d
kbys <work4kbys@gmail.com>
2021-09-22 22:56:25 +0900

    fix libSDL2main.a

--- pack.sh
+++ pack.sh
@@ -9,8 +9,8 @@ mkdir -p ${DIR}/lib ${DIR}/include ${DIR}/bin ${DIR}/licenses
 
 # SDL2
 cp -R ${SDL2}/x86_64-w64-mingw32/include ${DIR}/
-for i in libSDL2main.a libSDL2.dll.a; do
-  cp -R ${SDL2}/x86_64-w64-mingw32/lib/libSDL2.dll.a ${DIR}/lib/
+for dll in libSDL2main.a libSDL2.dll.a; do
+  cp -R ${SDL2}/x86_64-w64-mingw32/lib/${dll} ${DIR}/lib/
 done
 cp -R ${SDL2}/x86_64-w64-mingw32/bin/SDL2.dll ${DIR}/bin/
 cp -R ${SDL2}/COPYING.txt ${DIR}/licenses/COPYING-${SDL2}.txt
@@ -18,8 +18,8 @@ cp -R ${SDL2}/COPYING.txt ${DIR}/licenses/COPYING-${SDL2}.txt
 # SDL2_mixer
 cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/include ${DIR}/
 cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/lib/libSDL2_mixer.dll.a ${DIR}/lib/
-for i in SDL2_mixer.dll libmpg123-0.dll; do
-  cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/bin/$i ${DIR}/bin/
+for dll in SDL2_mixer.dll libmpg123-0.dll; do
+  cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/bin/${dll} ${DIR}/bin/
 done
 cp -R ${SDL2_MIXER}/COPYING.txt ${DIR}/licenses/COPYING-${SDL2_MIXER}.txt
 cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/bin/LICENSE.mpg123.txt ${DIR}/licenses/
@@ -27,8 +27,8 @@ cp -R ${SDL2_MIXER}/x86_64-w64-mingw32/bin/LICENSE.mpg123.txt ${DIR}/licenses/
 # SDL2_image
 cp -R ${SDL2_IMAGE}/x86_64-w64-mingw32/include ${DIR}/
 cp -R ${SDL2_IMAGE}/x86_64-w64-mingw32/lib/libSDL2_image.dll.a ${DIR}/lib/
-for i in SDL2_image.dll libpng16-16.dll zlib1.dll; do
-  cp -R ${SDL2_IMAGE}/x86_64-w64-mingw32/bin/$i ${DIR}/bin/
+for dll in SDL2_image.dll libpng16-16.dll zlib1.dll; do
+  cp -R ${SDL2_IMAGE}/x86_64-w64-mingw32/bin/${dll} ${DIR}/bin/
 done
 cp -R ${SDL2_IMAGE}/COPYING.txt ${DIR}/licenses/COPYING-${SDL2_IMAGE}.txt
 cp -R ${SDL2_IMAGE}/x86_64-w64-mingw32/bin/LICENSE.png.txt ${DIR}/licenses
